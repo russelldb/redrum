eed576554164cc4632077c213ee464038ac13bd9
Russell Brown <russell.brown@mac.com>
2017-04-11 14:18:00 +0100

    move main

---
+++ src/redrum_escript.erl
@@ -0,0 +1,79 @@
+-module(redrum_escript).
+
+%% API exports
+-export([main/1]).
+-compile(export_all).
+
+-define(REBAR_CONF, "rebar.config").
+-define(SCHEME_DEFAULTS, [{git, 9418}, {https, 443}, {ssh, 22}]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+%% escript Entry point
+main(Args) ->
+    {ParsedArgs, ExtraArgs} = case getopt:parse(cli_opts(), Args) of
+                                  {ok, {P, E}} -> {P, E};
+                                  _ -> usage()
+                              end,
+    io:format("Parsed Args ~p Opts ~p ~n", [ParsedArgs, ExtraArgs]),
+
+    case run_usage(ParsedArgs) of
+        true -> usage();
+        _ -> ok
+    end,
+
+    Conf = read_conf(proplists:get_value(config, ParsedArgs, undefined), ParsedArgs),
+
+    ok = maybe_remap_file(ParsedArgs, Conf),
+    ok = maybe_remap_deps(ParsedArgs, Conf),
+
+    erlang:halt(0).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+cli_opts() ->
+    [
+     {file,         $f,        "file",      {string, "rebar.config"}, "File to remap"},
+     {skip,         $s,        "skip",      undefined, "skip top level rebar config file"},
+     {deps,         $d,        "deps",      undefined, "remap default rebar deps directory 'deps'"},
+     {deps_dir,     $i,        "deps_dir",  string,  "Deps directory to remap [if not usual 'deps' directory]"},
+     {config,       $c,        "config",    {string, "redrum.conf"},   "config file containing remappings"},
+     {help,         $h,        "help",     undefined,                 "help / usage"}
+    ].
+
+usage() ->
+    getopt:usage(cli_opts(), "redrum"),
+    halt(0).
+
+run_usage([]) -> true;
+run_usage(ParsedArgs) ->
+    lists:member(help, ParsedArgs).
+
+read_conf(undefined, Args) ->
+    run_usage(Args);
+read_conf(File, _Args) ->
+    case file:consult(File) of
+        {ok, Conf} ->
+            Conf;
+        Error ->
+            io:format("Error ~p reading config file ~p~n", [Error, File]),
+            halt(0)
+    end.
+
+%% Only remap a file if asked to
+maybe_remap_file(Args, Conf) ->
+    Skip = proplists:get_value(skip, Args, false),
+    File = proplists:get_value(file, Args),
+    maybe_remap_file(Skip, File, Conf).
+
+maybe_remap_file(true, _, _Conf) ->
+    ok;
+maybe_remap_file(false, File, Conf) ->
+    redrum:remap_rebar_config(File, Conf, []).
+
+%% @TODO(implement)
+maybe_remap_deps(_Args, _Conf) ->
+    ok.
